db31af28aa035f837f3a9f25b22f010ae7d3637b
AijuiL <zx953159@gmail.com>
2017-04-02 13:17:19 +0800

    fix settings&gunicorn

--- pblog/settings.py
+++ pblog/settings.py
@@ -109,4 +109,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
